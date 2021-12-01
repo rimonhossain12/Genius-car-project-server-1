1040d4387ed9b0bcb3d5560dc3e26ce97e2e6642
rimonspi12 <rimonmpi@gmail.com>
2021-12-01 14:43:26 +0600

    some features added

--- index.js
+++ index.js
@@ -16,15 +16,14 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 // connecting database
 
-async function run (){
-    try{
+async function run() {
+    try {
         await client.connect();
-
         const database = client.db('car_project-2');
         const servicesCollection = database.collection('services');
 
         // GET API
-        app.post('/services',async(req,res) => {
+        app.post('/services', async (req, res) => {
             console.log('hitting the api');
             const service = req.body;
             const result = await servicesCollection.insertOne(service);
@@ -32,67 +31,62 @@ async function run (){
             res.json(result);
         });
 
-        app.get('/services',async(req,res) => {
+        app.get('/services', async (req, res) => {
             const cursor = servicesCollection.find({});
             const result = await cursor.toArray();
             res.json(result);
         });
 
         // GET a single service
-        app.get('/services/:id',async(req,res) => {
+        app.get('/services/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: ObjectId(id)};
+            const query = { _id: ObjectId(id) };
             const service = await servicesCollection.findOne(query);
             res.json(service);
         });
 
         // DELETE API
-        app.delete('/services/:id',async(req,res) => {
+        app.delete('/services/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id:ObjectId(id)};
+            const query = { _id: ObjectId(id) };
             const result = await servicesCollection.deleteOne(query);
-            console.log('data is deleted',result);
+            console.log('data is deleted', result);
             res.json(result);
         });
 
         // PUT API
-        app.put('/services/:id',async(req,res) => {
+        app.put('/services/:id', async (req, res) => {
             const id = req.params.id;
             const updatedUser = req.body;
-            console.log('hitting the api',updatedUser);
-
-            const filter = {_id: ObjectId(id)};
-            // const options = { upsert:true};
-
+            console.log('hitting the api', updatedUser);
+            const filter = { _id: ObjectId(id) };
             const updateDoc = {
                 $set: {
-                    img:updatedUser.IMG,
-                    name:updatedUser.name,
-                    price:updatedUser.price,
+                    img: updatedUser.IMG,
+                    name: updatedUser.name,
+                    price: updatedUser.price,
                     description: updatedUser.description
                 },
             };
             const result = await servicesCollection.updateOne(filter, updateDoc);
-            console.log('result found',result);
+            console.log('result found', result);
             console.log(
                 `${result.matchedCount} document(s) matched the filter, updated ${result.modifiedCount} document(s)`,
             );
             res.json(result);
-
         })
-
     }
-    finally{
+    finally {
         // await client.close();
     }
 }
 run().catch(console.dir);
 
 
-app.get('/',(req,res) => {
+app.get('/', (req, res) => {
     res.send('hello genius car server is running');
 })
 
-app.listen(port,() => {
-    console.log('port on running',port);
+app.listen(port, () => {
+    console.log('port on running', port);
 })
